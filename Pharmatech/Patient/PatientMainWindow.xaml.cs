ce4f9335329f3ebc30e5cc2dbfdcfb285bcd27ef
s2110 <s2110@DESKTOP-FR7RFN5>
2017-09-10 17:37:18 +0200

    Add validation to ONLY allow numeric input for contact numbers
    - ^^

--- Pharmatech/Patient/PatientMainWindow.xaml.cs
+++ Pharmatech/Patient/PatientMainWindow.xaml.cs
@@ -633,9 +633,27 @@ namespace Pharmatech
 
         }
 
-        
+        private void textBox_ContactNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Function on textchanged event, to on the fly not allow input OTHER than numerics.
 
-
+            System.Windows.Controls.TextBox textBox = sender as System.Windows.Controls.TextBox;
+            Int32 selectionStart = textBox.SelectionStart;
+            Int32 selectionLength = textBox.SelectionLength;
+            String newText = String.Empty;
+            int count = 0;
+            foreach (Char c in textBox.Text.ToCharArray())
+            {
+                if (Char.IsDigit(c) || Char.IsControl(c) || (c == '.' && count == 0))
+                {
+                    newText += c;
+                    if (c == '.')
+                        count += 1;
+                }
+            }
+            textBox.Text = newText;
+            textBox.SelectionStart = selectionStart <= textBox.Text.Length ? selectionStart : textBox.Text.Length;
+        }
     }
 
 }
